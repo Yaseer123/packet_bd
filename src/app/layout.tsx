c66fd7e1c44abbbf62cd2e0dd16e41df73c538f3
Yaseer123 <116842385+Yaseer123@users.noreply.github.com>
2025-07-15 08:01:02 +0600

    tag manager updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PX4XFQ2N');
+            })(window,document,'script','dataLayer','GTM-NQ56SVXT');
           `,
         }}
       />
@@ -39,7 +39,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PX4XFQ2N"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NQ56SVXT"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
