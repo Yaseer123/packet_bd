59a64fe8aa6f6f79baeed62a725ac4df7177e568
KaziMahbuburRahman <Mahbubbengali@gmail.com>
2025-08-08 18:41:25 +0600

    Refactor auth configuration to streamline imports and maintain consistency
    - Moved the import statement for environment variables to the top of the file for better organization.
    - Ensured consistent import structure, enhancing readability and maintainability of the auth configuration file.

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,9 +1,9 @@
+import { env } from "@/env";
 import { db } from "@/server/db";
 import { getUserById } from "@/utils/getUser";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type UserRole } from "@prisma/client";
 import bcrypt from "bcryptjs";
-import { env } from "@/env";
 
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
