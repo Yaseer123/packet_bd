6c7e2582137ea41ffc30cdeb4b50c66d83913d20
KaziMahbuburRahman <Mahbubbengali@gmail.com>
2025-08-08 18:39:24 +0600

    Update authConfig to utilize environment variables for Google authentication
    - Integrated environment variables for Google client ID and secret in the authConfig.
    - Improved security and flexibility by removing hardcoded credentials, allowing for easier configuration across different environments.

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -3,6 +3,7 @@ import { getUserById } from "@/utils/getUser";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type UserRole } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import { env } from "@/env";
 
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -49,6 +50,8 @@ declare module "next-auth/jwt" {
 export const authConfig = {
   providers: [
     GoogleProvider({
+      clientId: env.AUTH_GOOGLE_ID,
+      clientSecret: env.AUTH_GOOGLE_SECRET,
       allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
